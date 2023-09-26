fa18442a25fb8874631bc25e3a5a999b77dd160c
David120000 <david.remetei@gmail.com>
2023-09-26 19:46:42 +0200

    initial commit

---
+++ src/main/java/rd/sb_ng_browsergames/SbNgBrowsergamesApplication.java
@@ -0,0 +1,13 @@
+package rd.sb_ng_browsergames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbNgBrowsergamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbNgBrowsergamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rd/sb_ng_browsergames/SbNgBrowsergamesApplicationTests.java
@@ -0,0 +1,13 @@
+package rd.sb_ng_browsergames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbNgBrowsergamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
