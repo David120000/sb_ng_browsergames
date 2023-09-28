e42685051480f649935f3117d5139d53f037312f
David120000 <david.remetei@gmail.com>
2023-09-28 13:04:21 +0200

    Auth Response Entity added

---
+++ src/main/java/rd/sb_ng_browsergames/model/dto/AuthResponse.java
@@ -0,0 +1,22 @@
+package rd.sb_ng_browsergames.model.dto;
+
+public class AuthResponse {
+
+    private String jwt;
+
+
+    public AuthResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
+
+    
+}
--- src/main/java/rd/sb_ng_browsergames/service/AuthenticationService.java
+++ src/main/java/rd/sb_ng_browsergames/service/AuthenticationService.java
@@ -1,16 +1,14 @@
 package rd.sb_ng_browsergames.service;
 
-import java.net.http.HttpHeaders;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import rd.sb_ng_browsergames.model.dto.AuthRequest;
+import rd.sb_ng_browsergames.model.dto.AuthResponse;
 import rd.sb_ng_browsergames.service.utility.JwtUtil;
 import rd.sb_ng_browsergames.service.utility.UserDetailsObjProvider;
 
@@ -27,14 +25,14 @@ public class AuthenticationService {
     private JwtUtil jwtUtil;
 
 
-    public ResponseEntity<String> authenticate(AuthRequest request) throws Exception {
+    public ResponseEntity<AuthResponse> authenticate(AuthRequest request) throws Exception {
 
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
 
         UserDetails user = userObjProvider.loadUserByUsername(request.getUsername());
         String jwt = jwtUtil.generateToken(user);
 
-        return ResponseEntity.ok(jwt);
+        return ResponseEntity.ok(new AuthResponse(jwt));
       }
 
 
