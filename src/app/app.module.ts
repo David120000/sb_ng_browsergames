e7cede0dbc864e922cde9d6154b81e7495532fc1
David120000 <david.remetei@gmail.com>
2023-09-29 15:55:32 +0200

    basic frontend design + basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MinesweeperComponent } from './minesweeper/minesweeper.component';
+import { TictactoeComponent } from './tictactoe/tictactoe.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'minesweeper', component: MinesweeperComponent },
+  { path: 'tictactoe', component: TictactoeComponent },
+  { path: '',   redirectTo: '/', pathMatch: 'full' }, // main page
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MinesweeperComponent,
+    TictactoeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
