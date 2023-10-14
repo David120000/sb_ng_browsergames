390b7866711a4ab3312850acb4ed26ff4b9852dc
David120000 <david.remetei@gmail.com>
2023-10-14 23:45:38 +0200

    frontend router responsive menu + UI improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,41 @@
-import { AfterViewInit, Component, ElementRef, Renderer2 } from '@angular/core';
-import { ChildrenOutletContexts } from '@angular/router';
+import { AfterViewInit, Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { ChildrenOutletContexts, Router, RoutesRecognized } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'sb_ng_browsergames';
 
   private contexts: ChildrenOutletContexts;
   private elementRef: ElementRef;
   private renderer: Renderer2;
+  private router: Router;
+
+  private currentRoute: string;
 
 
-  constructor(contexts: ChildrenOutletContexts, elementRef: ElementRef, renderer: Renderer2) {
+  constructor(contexts: ChildrenOutletContexts, elementRef: ElementRef, renderer: Renderer2, router: Router) {
+   
     this.contexts = contexts;
     this.elementRef = elementRef;
     this.renderer = renderer;
+    this.router = router;
+    this.currentRoute = "";
+  }
+
+
+  public ngOnInit(): void {
+   
+    this.router.events.subscribe(data => {
+      
+      if(data instanceof RoutesRecognized) {
+        this.changeViewByRoute(data);
+      }
+
+    });
   }
 
 
@@ -54,6 +72,66 @@ export class AppComponent implements AfterViewInit {
     this.renderer.setStyle(navCardContainer, "opacity", "1");
 
     this.renderer.setStyle(routerOutletDiv, "opacity", "1");
+  }
+
+
+  private changeViewByRoute(route: RoutesRecognized) {
+
+    let currentRoute = route.state.root.firstChild?.data['route_id'];
+
+    if(currentRoute == "Minesweeper" || currentRoute == "Tictactoe") {
+      if(this.currentRoute != "Minesweeper" && this.currentRoute != "Tictactoe") {
+
+        this.minimizeGameMenu();
+        this.currentRoute = currentRoute;
+      }
+    }
+
+    if(currentRoute != "Minesweeper" && currentRoute != "Tictactoe") {
+      if(this.currentRoute == "Minesweeper" || this.currentRoute == "Tictactoe") {
+
+        this.maximizeGameMenu();
+        this.currentRoute = currentRoute;
+      }
+    }
+  }
+
+
+  private minimizeGameMenu() {
+
+    let cardcontainer = this.elementRef.nativeElement.querySelector("#card-container");
+    let tictactoecard = this.elementRef.nativeElement.querySelector("#tictactoecard");
+    let minesweepercard = this.elementRef.nativeElement.querySelector("#minesweepercard");
+
+    this.renderer.setStyle(cardcontainer, "margin-top", "6px");
+    this.renderer.setStyle(cardcontainer, "margin-bottom", "0px");
+
+    this.renderer.setStyle(tictactoecard, "height", "20px");
+    this.renderer.setStyle(tictactoecard, "padding", "6px");
+    this.renderer.setStyle(tictactoecard, "border-radius", "5px");
+
+    this.renderer.setStyle(minesweepercard, "height", "20px");
+    this.renderer.setStyle(minesweepercard, "padding", "6px");
+    this.renderer.setStyle(minesweepercard, "border-radius", "5px");
+  }
+
+
+  private maximizeGameMenu() {
+
+    let cardcontainer = this.elementRef.nativeElement.querySelector("#card-container");
+    let tictactoecard = this.elementRef.nativeElement.querySelector("#tictactoecard");
+    let minesweepercard = this.elementRef.nativeElement.querySelector("#minesweepercard");
+
+    this.renderer.setStyle(cardcontainer, "margin-top", "26px");
+    this.renderer.setStyle(cardcontainer, "margin-bottom", "30px");
+
+    this.renderer.setStyle(tictactoecard, "height", "78px");
+    this.renderer.setStyle(tictactoecard, "padding", "16px");
+    this.renderer.setStyle(tictactoecard, "border-radius", "15px");
+
+    this.renderer.setStyle(minesweepercard, "height", "78px");
+    this.renderer.setStyle(minesweepercard, "padding", "16px");
+    this.renderer.setStyle(minesweepercard, "border-radius", "15px");
   }
 
 }
